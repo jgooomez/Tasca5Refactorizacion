082ba22d4a6538a0bb44486f67b5bf885417d8ff
Javier Gomez Davo <javierillo756@gmail.com>
2023-05-22 21:08:04 +0200

    Clase principal arreglada y test4

--- SongTDD/src/TDDSong.java
+++ SongTDD/src/TDDSong.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class TDDSong {
-    public String generaCancion (ArrayList<String> listaAnimales) {
+    public String generaCancion(ArrayList<String> listaAnimales) {
         String cancion = "";
         String animal;
         String animalAnterior;
@@ -14,17 +14,22 @@ public class TDDSong {
             if (i > 0) {
                 cancion += "That wriggled and wiggled and tickled inside her.\n";
 
-                for (int j = i; j > 0 ; j--) {
+                for (int j = i; j > 0; j--) {
                     animalAnterior = listaAnimales.get(j - 1);
                     animalActual = listaAnimales.get(j);
 
-                    cancion += "She swallowed the " + animalAnterior + " to catch the " + animalActual + ";\n";
+                    cancion += "She swallowed the " + animalActual + " to catch the " + animalAnterior + ".\n";
                 }
             }
 
-            cancion += "I don't know why she swallowed a " + animal + " - perhaps she'll die!\n";
+            if (i < listaAnimales.size() - 1) {
+                cancion += "I don't know why she swallowed a " + animal + " - perhaps she'll die!\n";
+            } else {
+                cancion += "I don't know why she swallowed a " + listaAnimales.get(0) + " - perhaps she'll die!\n";
+            }
         }
 
         return cancion;
     }
+
 }
--- SongTDD/src/TDDSongTest.java
+++ SongTDD/src/TDDSongTest.java
@@ -31,4 +31,18 @@ class TDDSongTest {
         assertEquals(resultadoEsperado, resultadoActual);
     }
 
+    @org.junit.jupiter.api.Test
+    void testGeneraCancionFlySpider() {
+        listaAnimales.add("fly");
+        listaAnimales.add("spider");
+        String resultadoActual = song.generaCancion(listaAnimales);
+        String resultadoEsperado = "There was an old lady who swallowed a fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n"+
+                "There was an old lady who swallowed a spider.\n" +
+                "That wriggled and wiggled and tickled inside her.\n" +
+                "She swallowed the spider to catch the fly.\n" +
+                "I don't know why she swallowed a fly - perhaps she'll die!\n";
+        assertEquals(resultadoEsperado, resultadoActual);
+    }
+
 }
