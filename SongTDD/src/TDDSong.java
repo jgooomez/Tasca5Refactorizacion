270838ec2914976c6b5b5f0460e11cc08047fa7f
Javier Gomez Davo <javierillo756@gmail.com>
2023-05-22 20:41:06 +0200

    Clase sin test acabada

--- SongTDD/src/TDDSong.java
+++ SongTDD/src/TDDSong.java
@@ -5,7 +5,7 @@ public class TDDSong {
         String cancion = "";
         String animal;
         String animalAnterior;
-        String animalSiguiente;
+        String animalActual;
 
         for (int i = 0; i < listaAnimales.size(); i++) {
             animal = listaAnimales.get(i);
@@ -15,9 +15,16 @@ public class TDDSong {
                 cancion += "That wriggled and wiggled and tickled inside her.\n";
 
                 for (int j = i; j > 0 ; j--) {
+                    animalAnterior = listaAnimales.get(j - 1);
+                    animalActual = listaAnimales.get(j);
 
+                    cancion += "She swallowed the " + animalAnterior + " to catch the " + animalActual + ";\n";
                 }
             }
+
+            cancion += "I don't know why she swallowed a " + animal + " - perhaps she'll die!\n\n";
         }
+
+        return cancion;
     }
 }
